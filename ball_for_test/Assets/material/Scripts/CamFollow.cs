3327ba72cb7c42a7aec4dcaf2b8303eb6dd73945
goodahn <ahnjaeyeong@anjaeyeong-ui-MacBook-Pro.local>
2015-11-19 19:05:20 +0900

    ball for test

---
+++ ball_for_test/Assets/material/Scripts/CamFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform target;
+	public float dist=10f;
+	public float height=3f;
+	public float damptrace=20f;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Vector3.Lerp (transform.position
+		                                  , target.position - (target.forward * dist) + Vector3.up * height
+		                                  , Time.deltaTime * damptrace);
+		transform.LookAt (target.position);
+	}
+}
---
+++ ball_for_test/Assets/material/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+   
+    private Rigidbody rb;
+    public float speed;
+    private Vector3 mouse_p;
+/*    void Update() //before rendering code
+    {               // most of game code will go on
+        
+            mouse_p = new Vector3((((float)Input.mousePosition.x) - Screen.width / 2) * 0.05f, 0.0f, (Input.mousePosition.y * 0.05f - Screen.height / 2)) * 0.05f;
+            rb.MovePosition(mouse_p + rb.position);
+        
+    }*/
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()  // just before perform any physics
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //rb.AddForce(movement*speed);
+		transform.Translate (new Vector3 (moveHorizontal, 0, moveVertical) * speed * Time.deltaTime);
+		transform.Rotate (new Vector3 (Input.GetAxis("Mouse Y"), Input.GetAxis ("Mouse X"), 0) );
+
+    }
+
+    
+/*    void OnMouseDrag()
+    {
+        Vector3 mouseP = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 objP = Camera.main.ScreenToWorldPoint(mouseP);
+        transform.position = objP;
+        
+    }*/
+}
