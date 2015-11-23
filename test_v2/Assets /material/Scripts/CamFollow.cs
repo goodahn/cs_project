f9851471ec6cdd39c86d6f6877673da317644a16
goodahn <ahnjaeyeong@anjaeyeong-ui-MacBook-Pro.local>
2015-11-24 03:28:03 +0900

    fixed object, moving camera

---
+++ test_v2/Assets /material/Scripts/CamFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform target;
+	public float dist=10f;
+	public float height=3f;
+	public float damptrace=20f;
+	public float speed;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		float z_position = 0;
+		float z_rotation = 0;
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		if( Input.GetKey (KeyCode.Q)) {
+			z_position +=1;
+		}
+		if(Input.GetKey (KeyCode.Z)) {
+			z_position +=-1;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			z_rotation += 1;
+		}
+		if (Input.GetKey (KeyCode.C)) {
+			z_rotation +=-1;
+		}
+		transform.Translate (new Vector3 (moveHorizontal, z_position, moveVertical) * speed * Time.deltaTime);
+		transform.Rotate (new Vector3 (-Input.GetAxis("Mouse Y"), Input.GetAxis ("Mouse X"), z_rotation) );
+	}
+}
